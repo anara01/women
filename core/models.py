d47e091be81099bf35ccec99cf9650a6f0555151
anara01 <anaraerkebaeva2@gmail.com>
2022-09-17 16:11:32 +0300

    URL (slug) & page post

--- core/models.py
+++ core/models.py
@@ -4,18 +4,19 @@ from django.db import models
 
 class Women(models.Model):
     title = models.CharField("Заголовок", max_length=255)
+    slug = models.SlugField("URL", max_length=255, unique=True, db_index=True)
     content = models.TextField("Текст статьи", blank=True)
     photo = models.ImageField("Фото", upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField("Время создания", auto_now_add=True)
     time_update = models.DateTimeField("Время изменения", auto_now=True)
     is_published = models.BooleanField("Публикация", default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категории")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = "Известные женщины"
@@ -24,6 +25,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField("Категория", max_length=100, db_index=True)
+    slug = models.SlugField("URL", max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
