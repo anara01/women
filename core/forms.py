fdbe49dd3ccae72ceea3ffc39b85e92d018a3e5b
anara01 <anaraerkebaeva2@gmail.com>
2022-09-27 16:08:40 +0300

    login & logout

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,11 @@
+from multiprocessing import AuthenticationError
 from django import forms
 from django.core.exceptions import ValidationError
 
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -40,3 +42,9 @@ class RegisterUserForm(UserCreationForm):
 		#	'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
 		#	'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
 		#}
+
+
+class LoginUserForm(AuthenticationForm):
+	username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+	password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+	
