1fd8e73e8f16bd48423b23b01a6002b3c42b787c
anara01 <anaraerkebaeva2@gmail.com>
2022-09-27 15:02:07 +0300

    register

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -22,3 +24,19 @@ class AddPostForm(forms.ModelForm):
             raise ValidationError('Длина превышает 200 символов')
 
         return title
+
+
+class RegisterUserForm(UserCreationForm):
+	username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+	email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+	password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+	password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2')
+		#widgets = {
+		#	'username': forms.TextInput(attrs={'class': 'form-input'}),
+		#	'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+		#	'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+		#}
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
+import imp
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+# from django.contrib.auth.forms import UserCreationForm 
 
 from .forms import *
 from .models import *
@@ -135,3 +137,14 @@ class WomenCategory(DataMixin, ListView):
 #         'cat_selected': cat_id,
 #     }
 #     return render(request, 'core/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+	form_class = RegisterUserForm
+	template_name = 'core/register.html'
+	success_url = reverse_lazy('login')
+	
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		c_def = self.get_user_context(title='Регистрация')
+		return dict(list(context.items()) + list(c_def.items()))
